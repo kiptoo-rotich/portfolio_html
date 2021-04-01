d6c1aacc74ba754579c0390853b067555f6ac34d
Softonic254 <rotichenoch27@gmail.com>
2021-04-01 21:00:49 +0300

    It will add javascript file and verify inputs from user and gives the output

---
+++ contact.js
@@ -0,0 +1,17 @@
+function feedback() {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let message = document.querySelector("#message").value;
+    let radiobutton = document.getElementsByClassName("radio");
+
+
+    if (name && email && message !== "") {
+
+        alert("Your " + radiobutton.value + " response has been received. Thank you for your feedback. We'll be in touch with you using " + email)
+
+    } else {
+        alert("Kindly provide all details.")
+    }
+
+}
+
